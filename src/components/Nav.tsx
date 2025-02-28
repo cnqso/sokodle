4908d865d8d6f7258d46a8470f3ba1c93787d0cf
Will <wmkelly@umich.edu>
2025-02-27 20:50:01 -0500

    nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,117 +13,41 @@ import {
 } from '@/components/ui/navigation-menu'
 
 import { cn } from '@/lib/utils'
+import { Link } from '@radix-ui/react-navigation-menu'
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Alert Dialog',
-    href: 'https://ui.shadcn.com/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
-  },
-  {
-    title: 'Hover Card',
-    href: 'https://ui.shadcn.com/docs/primitives/hover-card',
-    description:
-      'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: 'https://ui.shadcn.com/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: 'https://ui.shadcn.com/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: 'https://ui.shadcn.com/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: 'https://ui.shadcn.com/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-  },
-]
+
 
 export default function Nav() {
   return (
     <NavigationMenu className="z-[5] m750:max-w-[300px] grow-0 mb-5">
       <NavigationMenuList className="m750:max-w-[300px]">
         <NavigationMenuItem>
-          <NavigationMenuTrigger className="m750:max-w-[80px] m750:text-xs">
-            <span className="m750:hidden">Getting started</span>
-            <span className="hidden m750:inline">Home</span>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[500px] gap-3 p-6 lg:grid-cols-[.75fr_1fr] m750:w-[300px]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="from-muted/50 to-muted flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none focus:shadow-md"
-                    href="https://ui.shadcn.com"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-heading">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm font-base leading-tight">
-                      Beautifully designed components that you can copy and
-                      paste into your apps. Accessible. Customizable. Open
-                      Source.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="https://ui.shadcn.com/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem
-                href="https://ui.shadcn.com/docs/installation"
-                title="Installation"
-              >
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem
-                href="https://ui.shadcn.com/docs/primitives/typography"
-                title="Typography"
-              >
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="m750:max-w-[80px] m750:text-xs">
-            Components
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <a href="https://ui.shadcn.com/docs" target='blank'>
+          <Link href="/">
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               <span className="m750:max-w-[80px] m750:text-xs">
-                Documentation
+                Home
               </span>
             </NavigationMenuLink>
-          </a>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/editor">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <span className="m750:max-w-[80px] m750:text-xs">
+                Level Creator
+              </span>
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <Link href="/userlevels">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <span className="m750:max-w-[80px] m750:text-xs">
+                User levels
+              </span>
+            </NavigationMenuLink>
+          </Link>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
